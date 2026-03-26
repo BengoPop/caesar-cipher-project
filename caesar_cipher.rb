1eab868d8f675a291fb8c6c6ac3cfb4612e4c722
BengoPop <retr0astr4@gmail.com>
2026-03-26 10:43:41 -0400

    refactored and added functions

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,13 +1,22 @@
 # add a method to keep the uppercase if uppercase
-def shift_chars_with_case(input, int_shift_factor)
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+def encode_shift_with_case(input, int_shift_factor, alphabet)
   input_array = input.split('')
   input_array.map do |letter|
     if alphabet.include?(letter.downcase)
       shifted_index = (alphabet.index(letter.downcase) + int_shift_factor) % alphabet.length
-      { shifted_index => { is_upper?: letter == letter.upcase } }
+      [shifted_index, { is_upper?: letter == letter.upcase }]
     else
-      { letter => { is_upper?: false } }
+      [letter, { is_upper?: false }]
+    end
+  end
+end
+
+def assemble_shifted_string(array, alphabet)
+  array.reduce('') do |acc, (object, value)|
+    if object.is_a?(Integer)
+      value[:is_upper?] ? acc + alphabet[object].upcase : acc + alphabet[object]
+    else
+      acc + object
     end
   end
 end
@@ -15,15 +24,7 @@ end
 # change to map instead
 def caesar_cipher(string, int_shift_factor)
   alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  input_hash_shifted = shift_chars_with_case(string, int_shift_factor)
-  input_hash_shifted.reduce('') do |_acc, value|
-    value.each do |key, key_value|
-      if key.is_a?(Integer)
-        puts key_value[:isupper?] ? alphabet[key].upcase : alphabet[key]
-      else
-        puts key
-      end
-    end
-  end
+  input_hash_shifted = encode_shift_with_case(string, int_shift_factor, alphabet)
+  assemble_shifted_string(input_hash_shifted, alphabet)
 end
 puts caesar_cipher('Abc.', 1)
