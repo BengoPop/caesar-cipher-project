cf606168f0640616b8d90df44ab488c37931fb34
BengoPop <retr0astr4@gmail.com>
2026-03-26 03:17:51 -0400

    changed

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,29 @@
 # add a method to keep the uppercase if uppercase
-def caesar_cipher(string, int_shift_factor)
+def shift_chars_with_case(input, int_shift_factor)
   alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  input_array = string.split('')
-
-  input_array_shifted = input_array.map do |letter|
+  input_array = input.split('')
+  input_array.map do |letter|
     if alphabet.include?(letter.downcase)
-      (alphabet.index(letter) + int_shift_factor) % alphabet.length
+      shifted_index = (alphabet.index(letter.downcase) + int_shift_factor) % alphabet.length
+      { shifted_index => { is_upper?: letter == letter.upcase } }
     else
-      letter
+      { letter => { is_upper?: false } }
     end
   end
-  puts input_array_shifted
 end
 
-puts caesar_cipher('abc !', 1)
+# change to map instead
+def caesar_cipher(string, int_shift_factor)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  input_hash_shifted = shift_chars_with_case(string, int_shift_factor)
+  input_hash_shifted.reduce('') do |_acc, value|
+    value.each do |key, key_value|
+      if key.is_a?(Integer)
+        puts key_value[:isupper?] ? alphabet[key].upcase : alphabet[key]
+      else
+        puts key
+      end
+    end
+  end
+end
+puts caesar_cipher('Abc.', 1)
